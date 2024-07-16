b24535976d167adf9a5371c3ad1a2397c0780863
Sina Naeimi <snaeimi@udel.edu>
2024-07-15 19:12:53 -0700

    fixed some bugs and added one function for getting all data
    Data was not being loaded in functions in output/raw_data.py. Alsso added one function for returnning a wntr style result file.

--- rewet/Output/Raw_Data.py
+++ rewet/Output/Raw_Data.py
@@ -8,57 +8,65 @@ Created on Mon Oct 24 18:27:03 2022
 class Raw_Data():
     def __init__():
         pass
-    
+
     def saveDetailedDemandNodeData(self, scn_name, data_type, file_address, file_type):
+        self.loadScneariodata(scn_name)
         if data_type not in ['pressure','head','demand','quality']:
             raise ValueError("data type is not recognized for demand nodes: "+repr(data_type))
         data = self.getDetailedData(scn_name, data_type)
         data = data[self.demand_node_name_list]
         self.saveDataFrame(data, file_address, file_type=file_type)
-    
+
     def saveDetailedJunctionData(self, scn_name, data_type, file_address, file_type):
+        self.loadScneariodata(scn_name)
         if data_type not in ['pressure','head','demand','quality']:
             raise ValueError("data type is not recognized for junctiosn: "+repr(data_type))
         data = self.getDetailedData(scn_name, data_type)
         data = data[self.wn.junction_name_list]
         self.saveDataFrame(data, file_address, file_type=file_type)
-    
+
     def saveDetailedTankData(self, scn_name, data_type, file_address, file_type):
+        self.loadScneariodata(scn_name)
         if data_type not in ['pressure','head','demand','quality']:
             raise ValueError("data type is not recognized for tanks: "+repr(data_type))
         data = self.getDetailedData(scn_name, data_type)
         data = data[self.wn.tank_name_list]
         self.saveDataFrame(data, file_address, file_type=file_type)
-    
+
     def saveDetailedReservoirData(self, scn_name, data_type, file_address, file_type):
+        self.loadScneariodata(scn_name)
         if data_type not in ['pressure','head','demand','quality']:
             raise ValueError("data type is not recognized for demand nodes: "+repr(data_type))
         data = self.getDetailedData(scn_name, data_type)
         data = data[self.wn.tank_name_list]
         self.saveDataFrame(data, file_address, file_type=file_type)
-    
+
     def saveDetailedPipeData(self, scn_name, data_type, file_address, file_type):
+        self.loadScneariodata(scn_name)
         if data_type not in ['linkquality', 'flowrate', 'headloss', 'velocity', 'status', 'setting', 'frictionfact', 'rxnrate']:
             raise ValueError("data type is not recognized for pipes: "+repr(data_type))
         data = self.getDetailedData(scn_name, data_type)
         data = data[self.wn.pipe_name_list]
         self.saveDataFrame(data, file_address, file_type=file_type)
-    
+
     def saveDetailedPumpData(self, scn_name, data_type, file_address, file_type):
+        self.loadScneariodata(scn_name)
         if data_type not in ['linkquality', 'flowrate', 'headloss', 'velocity', 'status', 'setting', 'frictionfact', 'rxnrate']:
             raise ValueError("data type is not recognized for pumps: "+repr(data_type))
         data = self.getDetailedData(scn_name, data_type)
         data = data[self.wn.pump_name_list]
         self.saveDataFrame(data, file_address, file_type=file_type)
-    
+
     def saveDetailedValveData(self, scn_name, data_type, file_address, file_type):
+        self.loadScneariodata(scn_name)
         if data_type not in ['linkquality', 'flowrate', 'headloss', 'velocity', 'status', 'setting', 'frictionfact', 'rxnrate']:
             raise ValueError("data type is not recognized for valves: "+repr(data_type))
         data = self.getDetailedData(scn_name, data_type)
         data = data[self.wn.valve_name_list]
         self.saveDataFrame(data, file_address, file_type=file_type)
-   
+
     def getDetailedData(self, scn_name, data_type):
+        self.loadScneariodata(scn_name)
         cur_scn_data = None
         if data_type in ['linkquality', 'flowrate', 'headloss', 'velocity', 'status', 'setting', 'frictionfact', 'rxnrate']:
             cur_scn_data = self.data[scn_name].link[data_type]
@@ -68,6 +76,13 @@ class Raw_Data():
             raise ValueError("Unknown Data Type For output")
         return cur_scn_data
 
+    def getAllDetailedData(self, scn_name):
+        self.loadScneariodata(scn_name)
+        if scn_name not in self.data:
+            raise ValueError(f"Scneratio {scn_name} not in the result")
+
+        return self.data[scn_name]
+
     def saveDataFrame(dataframe , file_address, file_type='xlsx'):
         if file_type == 'xlsx':
             dataframe.to_excel(file_address)
@@ -75,4 +90,3 @@ class Raw_Data():
             dataframe.to_csv(file_address)
         else:
             raise ValueError("Unknown file type: "+repr(file_type))
-        
