b330a63ea7d7aa24d7f5e3c85b01cf1c0b646e4e
Sina Naeimi <snaeimi@udel.edu>
2024-07-15 19:14:37 -0700

    refactored project (moved one function from initial to project)
    In fact the function has been moved from initial to the project. In this way intiial will be smaler and the function will be in its pertinent porsition.

--- rewet/Project.py
+++ rewet/Project.py
@@ -5,7 +5,27 @@ Created on Mon Jan  9 09:03:57 2023
 @author: snaeimi
 """
 
+import os
+import pickle
+from rewet.Input.Input_IO import resolve_path
+
 class Project():
     def __init__(self, project_settings, scenario_list):
         self.scenario_list    = scenario_list
         self.project_settings = project_settings
+
+    @classmethod
+    def createProjectFile(cls,
+                          project_settings,
+                          damage_list,
+                          project_file_name):
+
+        project = cls(project_settings, damage_list)
+        project_file_addr = os.path.join(
+            project_settings.process['result_directory'],
+            project_file_name)
+
+        # resolve the project file path
+        project_file_addr = resolve_path(project_file_addr)
+        with open(project_file_addr, 'wb') as f:
+            pickle.dump(project, f)
